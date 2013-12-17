ce8c5fffa8ec3921bd837b58c3f1c99ae916bca5
Serhey Dolgushev <serhey.dolgushev@gmail.com>
2013-12-18 00:55:43 +0200

    fix clone

--- modules/export_translations/clone.php
+++ modules/export_translations/clone.php
@@ -11,6 +11,11 @@ if( $job instanceof TranslationExportJob === false ) {
 }
 
 $job->setAttribute( 'id', null );
+$job->setAttribute( 'file', null );
+$job->setAttribute( 'status', TranslationExportJob::STATUS_INITIALIZED );
+$job->setAttribute( 'creator_id', eZUser::currentUserID() );
+$job->setAttribute( 'created_at', time() );
+
 eZHTTPTool::instance()->setSessionVariable( 'new_export_job', $job );
 
 return $Params['Module']->redirectToView( 'export' );
